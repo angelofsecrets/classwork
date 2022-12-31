6d1c25795b46a833095b1f5e5aac8c0075c01a93
angelofsecrets <121728481+angelofsecrets@users.noreply.github.com>
2022-12-31 17:13:24 -0500

    Add files via upload

---
+++ _My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # "My Jupyter Notebook on IBM Watson Studio"
+
+# **Levi Scott** <br>
+# Robotics technician
+
+# *I am interested in data science because data is everywhere and a big booming field and want to get into it.*
+
+# ### this will do some basic math operations
+
+# In[6]:
+
+
+1+1, 5*2, 10-5, 20/2
+
+
+# * dogs
+# * cats
+# * birds
+# <img src="https://www.northeastern.edu/graduate/blog/wp-content/uploads/2020/06/iStock-1221293664-1-1-1.jpg" alt="picture of various graphs " title="data" />
+# 1. science
+# 2. data
+# 3. coding
+# 
+
+# In[ ]:
+
+
+
+
